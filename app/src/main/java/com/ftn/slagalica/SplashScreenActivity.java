4f926240203339019cb36fdb2be265453ee45bbe
2Dan3 <dan23.ftn@gmail.com>
2023-04-23 18:37:43 +0200

    Add Splash Screen Activity

---
+++ app/src/main/java/com/ftn/slagalica/SplashScreenActivity.java
@@ -0,0 +1,39 @@
+package com.ftn.slagalica;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.ftn.slagalica.databinding.ActivitySplashScreenBinding;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    public static final int SPLASH_TIMEOUT = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+                finish();
+            }
+        }, SPLASH_TIMEOUT);
+    }
+}
